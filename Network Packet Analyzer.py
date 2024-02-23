35c27c568d93d003748c420db1eaac866fe5dc86
Tarek Ibrahim <125945407+tarek-ibr@users.noreply.github.com>
2024-02-23 03:51:37 -1000

    Network Packet Analyzer.py

--- Network Packet Analyzer.py
+++ Network Packet Analyzer.py
@@ -1,27 +1,29 @@
 from scapy.all import *
 import netifaces
 
+
 # Function to analyze packets
-def analyzer(packet):
+def packet_analyze(packet):
+
     if IP in packet:
-        ip_src = packet[IP].src
-        ip_dst = packet[IP].dst
+        source_ip = packet[IP].src
+        destination_ip = packet[IP].dst
         
         if TCP in packet:
             # TCP packet
             dport = packet[TCP].dport
-            payload_size = len(packet[TCP].payload)
-            print(f"TCP  -> from IP = {ip_src}:{packet[TCP].sport} To IP = {ip_dst}:{dport}   size: {payload_size} bytes AND the Packet is \n{packet[TCP].payload}\n")
+            sizePayload = len(packet[TCP].payload)
+            print(f"TCP  -> from IP = {source_ip}:{packet[TCP].sport} To IP = {destination_ip}:{dport}   size: {sizePayload} bytes AND the Packet is \n{packet[TCP].payload}\n")
         
         elif UDP in packet:
             # UDP packet
             dport = packet[UDP].dport
-            payload_size = len(packet[UDP].payload)
-            print(f"UDP  -> from IP = {ip_src}:{packet[UDP].sport} To IP = {ip_dst}:{dport}   size: {payload_size} bytes AND the Packet is \n{packet[UDP].payload}\n")
+            sizePayload = len(packet[UDP].payload)
+            print(f"UDP  -> from IP = {source_ip}:{packet[UDP].sport} To IP = {destination_ip}:{dport}   size: {sizePayload} bytes AND the Packet is \n{packet[UDP].payload}\n")
         
         else:
             # IP packet without TCP or UDP
-            print(f"IP -> from IP = {ip_src} To IP = {ip_dst}")
+            print(f"IP -> from IP = {source_ip} To IP = {destination_ip}")
 
 # Get the list of network interfaces
 NET = netifaces.interfaces()
@@ -32,12 +34,12 @@ for i in NET:
         print(i)
 
 # User input for the interface to analyze
-ifaceName = input("Enter the Interface You want to analyze **Note** THE EXACT NAME:\n")
+ifaceName = input("what is the Interface You want to analyze ENTER THE EXACT NAME:\n")
 
 # Check if the entered interface exists
 if ifaceName in NET:
-    print("^_^ packet sniffer ^_^")
-    # Sniff packets on the specified interface and call the 'analyzer' function
-    sniff(iface=ifaceName, prn=analyzer)
+    print(" packet sniffer ")
+    # Sniff packets on the specified interface and call the 'packet_analyze' function
+    sniff(iface=ifaceName, prn=packet_analyze)
 else:
-    print("IFACE NOT FOUND TRY AGAIN")
+    print("the Interface is not found")
